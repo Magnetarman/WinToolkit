fa15b4567e77fcbf02b3635598d9c8cabb5a3480
Magnetar Man <40738529+Magnetarman@users.noreply.github.com>
2025-09-05 13:30:16 +0200

    Update WinToolkit.ps1

--- WinToolkit.ps1
+++ WinToolkit.ps1
@@ -5,11 +5,11 @@
     Questo script funge da menu principale per un insieme di strumenti di manutenzione e gestione di Windows.
     Permette agli utenti di selezionare ed eseguire vari script PowerShell per compiti specifici.
 .NOTES
-  Versione 2.0 (Build 66) - 2025-09-04
+  Versione 2.0 (Build 67) - 2025-09-04
 #>
 
 # Imposta il titolo della finestra di PowerShell per un'identificazione immediata.
-$Host.UI.RawUI.WindowTitle = "WinToolkit by MagnetarMan v2.0 (Build 66)"
+$Host.UI.RawUI.WindowTitle = "WinToolkit by MagnetarMan v2.0 (Build 67)"
 
 # Imposta una gestione degli errori più rigorosa per lo script.
 # 'Stop' interrompe l'esecuzione in caso di errore, permettendo una gestione controllata tramite try/catch.
@@ -442,15 +442,13 @@ $Host.UI.RawUI.WindowTitle = "Repair Toolkit By MagnetarMan"
         '    \_/\_/    |_||_| \_|'
         ''
         '    Repair Toolkit By MagnetarMan'
-        '      Version 2.0 (Build 12)'
+        '      Version 2.0 (Build 13)'
     )
     foreach ($line in $asciiArt) {
         Write-StyledMessage 'Info' (Center-Text -Text $line -Width $width)
     }
     Write-Host '' # Spazio
 
-
-Request-AdminRights
 
 # Countdown preparazione
 for ($i = 5; $i -gt 0; $i--) {
@@ -495,7 +493,7 @@ while ($true) {
         '    \_/\_/    |_||_| \_|'
         ''
         '    Toolkit By MagnetarMan'
-        '      Version 2.0 (Build 66)'
+        '      Version 2.0 (Build 67)'
     )
     foreach ($line in $asciiArt) {
         Write-StyledMessage 'Info' (Center-Text -Text $line -Width $width)
