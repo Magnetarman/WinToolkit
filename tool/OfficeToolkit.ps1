8db3682efc3d0c8b18da2a582d252dea5d8d8731
Magnetar Man <40738529+Magnetarman@users.noreply.github.com>
2025-09-18 22:04:15 +0200

    Update OfficeToolkit.ps1

--- tool/OfficeToolkit.ps1
+++ tool/OfficeToolkit.ps1
@@ -318,7 +318,7 @@ function OfficeToolkit {
         '         \_/\_/    |_||_| \_|',
         '',
         '     Office Toolkit By MagnetarMan',
-        '        Version 2.1 (Build 11)'
+        '        Version 2.1 (Build 13)'
     )
     $asciiArt | ForEach-Object { 
         $padding = [math]::Max(0, [math]::Floor(($width - $_.Length) / 2))
