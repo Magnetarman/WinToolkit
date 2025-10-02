e793eca75ffc7bed901779dbe4005c43219abebe
Magnetar Man <40738529+Magnetarman@users.noreply.github.com>
2025-10-02 14:57:23 +0200

    Update WinReinstallStore.ps1

--- tool/WinReinstallStore.ps1
+++ tool/WinReinstallStore.ps1
@@ -49,7 +49,7 @@ function WinReinstallStore {
             '         \_/\_/    |_||_| \_|',
             '',
             ' Store Repair Toolkit By MagnetarMan',
-            '       Version 2.2 (Build 30)'
+            '       Version 2.2.2 (Build 2)'
         )
 
         foreach ($line in $asciiArt) {
