a1ca52bdfb43e48697d20f3d12f730d537e555b8
Magnetar Man <40738529+Magnetarman@users.noreply.github.com>
2026-01-04 17:01:01 +0100

    Update WinToolkit-template.ps1

--- WinToolkit-template.ps1
+++ WinToolkit-template.ps1
@@ -14,7 +14,7 @@ param([int]$CountdownSeconds = 30)
 # --- CONFIGURAZIONE GLOBALE ---
 $ErrorActionPreference = 'Stop'
 $Host.UI.RawUI.WindowTitle = "WinToolkit by MagnetarMan"
-$ToolkitVersion = "2.5.0 (Build 159)"
+$ToolkitVersion = "2.5.0 (Build 160)"
 
 
 # Setup Variabili Globali UI
