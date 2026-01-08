ac8204c50ccbc0c79dc7e42627219f2b2159cbe9
Magnetar Man <40738529+Magnetarman@users.noreply.github.com>
2026-01-08 01:32:04 +0100

    Update Microsoft.PowerShell_profile.ps1

--- asset/Microsoft.PowerShell_profile.ps1
+++ asset/Microsoft.PowerShell_profile.ps1
@@ -121,11 +121,11 @@ function FlushDns {
 # ============================================================================
 
 function WinToolkit-Stable {
-    Start-Process -FilePath "https://magnetarman.com/WinToolkit"
+    Start-Process -FilePath "pwsh.exe" -ArgumentList "-ExecutionPolicy Bypass -Command \"irm https://magnetarman.com/WinToolkit | iex\"" -Verb RunAs
 }
 
 function WinToolkit-Dev {
-    Start-Process -FilePath "https://magnetarman.com/WinToolkit-Dev"
+    Start-Process -FilePath "pwsh.exe" -ArgumentList "-ExecutionPolicy Bypass -Command \"irm https://magnetarman.com/WinToolkit-Dev | iex\"" -Verb RunAs
 }
 
 # ============================================================================
