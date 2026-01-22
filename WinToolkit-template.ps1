e08505b1d2f572fc972e8e4134d26b4f8830f3fc
Magnetarman <40738529+Magnetarman@users.noreply.github.com>
2026-01-22 13:44:46 +0000

    🔖 Bump WinToolkit version to 2.5.0 (Build 230)
    • Incremento automatico numero build
    • Versione aggiornata nel template

--- WinToolkit-template.ps1
+++ WinToolkit-template.ps1
@@ -14,7 +14,7 @@ param([int]$CountdownSeconds = 30, [switch]$ImportOnly)
 # --- CONFIGURAZIONE GLOBALE ---
 $ErrorActionPreference = 'Stop'
 $Host.UI.RawUI.WindowTitle = "WinToolkit by MagnetarMan"
-$ToolkitVersion = "2.5.0 (Build 229)"
+$ToolkitVersion = "2.5.0 (Build 230)"
 
 # --- CONFIGURAZIONE CENTRALIZZATA ---
 $AppConfig = @{
@@ -624,4 +624,5 @@ else {
     # Esponi $menuStructure globalmente per la GUI
     $Global:menuStructure = $menuStructure
 }
+
 
