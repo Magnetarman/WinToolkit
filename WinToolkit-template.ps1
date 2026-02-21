9059250aff5a950299172425b778e22e20a31149
Magnetar Man <40738529+Magnetarman@users.noreply.github.com>
2026-02-21 23:54:33 +0100

    Update WinToolkit-template.ps1

--- WinToolkit-template.ps1
+++ WinToolkit-template.ps1
@@ -287,6 +287,7 @@ function Invoke-WithSpinner {
             }
 
             if (-not $result.HasExited) {
+                if (-not $Global:GuiSessionActive) { Write-Host "" } # Forza il ritorno a capo, chiudendo la riga dello spinner
                 Write-StyledMessage -Type 'Warning' -Text "Timeout raggiunto dopo $TimeoutSeconds secondi, terminazione processo..."
                 $result.Kill()
                 Start-Sleep -Seconds 2
