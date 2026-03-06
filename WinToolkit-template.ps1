50b07bc68423d9de3b8954ace828e0b53c0be933
Magnetar Man <40738529+Magnetarman@users.noreply.github.com>
2026-03-06 21:33:31 +0100

    Update WinToolkit-template.ps1

--- WinToolkit-template.ps1
+++ WinToolkit-template.ps1
@@ -13,7 +13,7 @@ param([int]$CountdownSeconds = 30, [switch]$ImportOnly)
 # --- CONFIGURAZIONE GLOBALE ---
 $ErrorActionPreference = 'Stop'
 $Host.UI.RawUI.WindowTitle = "WinToolkit by MagnetarMan"
-$ToolkitVersion = "2.5.2 (Build 16)"
+$ToolkitVersion = "2.5.2 (Build 18)"
 
 # --- CONFIGURAZIONE CENTRALIZZATA ---
 $AppConfig = @{
@@ -642,15 +642,3 @@ else {
     # Esponi $menuStructure globalmente per la GUI
     $Global:menuStructure = $menuStructure
 }
-
-
-
-
-
-
-
-
-
-
-
-
