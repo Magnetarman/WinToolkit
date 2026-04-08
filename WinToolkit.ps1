327ad151225df54a6131fa5382bde06124ca0202
Magnetarman <40738529+Magnetarman@users.noreply.github.com>
2026-04-08 21:45:21 +0000

    ✅ Compilato WinToolkit v2.5.4 (Build 4)

--- WinToolkit.ps1
+++ WinToolkit.ps1
@@ -70,7 +70,7 @@ function Read-Host {
 }
 $ErrorActionPreference = 'Stop'
 $Host.UI.RawUI.WindowTitle = "WinToolkit by MagnetarMan"
-$ToolkitVersion = "2.5.4 (Build 2)"
+$ToolkitVersion = "2.5.4 (Build 4)"
 $AppConfig = @{
     URLs     = @{
         GitHubAssetBaseUrl    = "https://raw.githubusercontent.com/Magnetarman/WinToolkit/main/asset/"
