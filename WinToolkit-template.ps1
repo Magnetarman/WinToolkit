328802cf4e34222e6fcb43cd437a8486dbbafb54
Magnetarman <40738529+Magnetarman@users.noreply.github.com>
2026-05-08 00:30:14 +0000

    🆙 Bump versione v2.5.4 (Build 40)

--- WinToolkit-template.ps1
+++ WinToolkit-template.ps1
@@ -97,7 +97,7 @@ function Read-Host {
 # --- CONFIGURAZIONE GLOBALE ---
 $ErrorActionPreference = 'Stop'
 $Host.UI.RawUI.WindowTitle = "WinToolkit by MagnetarMan"
-$ToolkitVersion = "2.5.4 (Build 39)"
+$ToolkitVersion = "2.5.4 (Build 40)"
 
 # --- CONFIGURAZIONE CENTRALIZZATA ---
 $AppConfig = @{
@@ -1974,6 +1974,7 @@ else {
     # Esponi $menuStructure globalmente per la GUI
     $Global:menuStructure = $menuStructure
 }
+
 
 
 
