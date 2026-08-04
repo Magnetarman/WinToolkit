56882396b5de64022c2991d4f22fcbedc69bf60b
Magnetarman <40738529+Magnetarman@users.noreply.github.com>
2026-08-05 00:35:22 +0200

    Update DisableBitlocker.ps1

--- tools/DisableBitlocker.ps1
+++ tools/DisableBitlocker.ps1
@@ -9,7 +9,7 @@ function DisableBitlocker {
         [switch]$SuppressIndividualReboot
     )
 
-    Start-ToolkitSession -ToolName "DisableBitlocker" -SubTitle (Get-Loc 'script.DisableBitlocker')
+    Start-ToolkitSession -ToolName "DisableBitlocker" -SubTitle (Get-SourceTextLoc 'script.DisableBitlocker')
 
     $regPath = $AppConfig.Registry.BitLocker
     $timeout = 3600
@@ -18,40 +18,40 @@ function DisableBitlocker {
         param([string]$DriveLetter = "C:")
         try { return manage-bde -status $DriveLetter }
         catch {
-            Write-StyledMessage -Type 'Warning' -Text (Get-Loc 'toolText.unableToCheckBitlockerStatus0' -Args @($($_.Exception.Message)))
+            Write-StyledMessage -Type 'Warning' -Text (Get-SourceTextLoc 'toolText.unableToCheckBitlockerStatus0' -Args @($($_.Exception.Message)))
             return $null
         }
     }
 
     try {
-        Write-StyledMessage -Type 'Info' -Text (Get-Loc 'toolText.initializingDriveCDecryption')
+        Write-StyledMessage -Type 'Info' -Text (Get-SourceTextLoc 'toolText.initializingDriveCDecryption')
 
-        $result = Invoke-WithSpinner -Activity (Get-Loc 'uiText.disablingBitlocker') -Command 'manage-bde.exe' `
+        $result = Invoke-WithSpinner -Activity (Get-SourceTextLoc 'uiText.disablingBitlocker') -Command 'manage-bde.exe' `
             -Arguments @('-off', 'C:') -TimeoutSeconds $timeout -LogContextKey "Bitlocker-Disable"
 
         if ($result.ExitCode -eq 0) {
-            Write-StyledMessage -Type 'Success' -Text (Get-Loc 'toolText.decryptionStartedCompletedSuccessfully')
+            Write-StyledMessage -Type 'Success' -Text (Get-SourceTextLoc 'toolText.decryptionStartedCompletedSuccessfully')
             Start-Sleep -Seconds 2
             $status = Test-BitLockerStatus -DriveLetter "C:"
             if ($status -match "Decryption in progress" -or $status -match 'Decryption in progress.') {
-                Write-StyledMessage -Type 'Info' -Text (Get-Loc 'toolText.decryptionInProgressInBackground')
+                Write-StyledMessage -Type 'Info' -Text (Get-SourceTextLoc 'toolText.decryptionInProgressInBackground')
             }
         }
         else {
-            Write-StyledMessage -Type 'Warning' -Text (Get-Loc 'toolText.manageBdeExitCode0BitlockerMayAlreadyBeDownOrInError' -Args @($($result.ExitCode)))
+            Write-StyledMessage -Type 'Warning' -Text (Get-SourceTextLoc 'toolText.manageBdeExitCode0BitlockerMayAlreadyBeDownOrInError' -Args @($($result.ExitCode)))
         }
 
-        Write-StyledMessage -Type 'Info' -Text (Get-Loc 'toolText.disablingAutomaticEncryptionInTheRegistry')
+        Write-StyledMessage -Type 'Info' -Text (Get-SourceTextLoc 'toolText.disablingAutomaticEncryptionInTheRegistry')
         Set-RegistryValue -Path $regPath -Name "PreventDeviceEncryption" -Value 1
 
-        Write-StyledMessage -Type 'Success' -Text (Get-Loc 'toolText.setupComplete')
+        Write-StyledMessage -Type 'Success' -Text (Get-SourceTextLoc 'toolText.setupComplete')
     }
     catch {
         Write-ToolkitError -Record $_ -ToolName "DisableBitlocker"
     }
     finally {
-        Write-StyledMessage -Type 'Info' -Text (Get-Loc 'toolText.resourceCleanupCompleted')
-        Invoke-ToolkitReboot -Message (Get-Loc 'toolText.extra.rebootingIn') -Seconds $CountdownSeconds -SuppressIndividualReboot:$SuppressIndividualReboot
-        Write-ToolkitLog -Level INFO -Message (Get-Loc 'toolText.disablebitlockerSessionEnded')
+        Write-StyledMessage -Type 'Info' -Text (Get-SourceTextLoc 'toolText.resourceCleanupCompleted')
+        Invoke-ToolkitReboot -Message (Get-SourceTextLoc 'toolText.extra.rebootingIn') -Seconds $CountdownSeconds -SuppressIndividualReboot:$SuppressIndividualReboot
+        Write-ToolkitLog -Level INFO -Message (Get-SourceTextLoc 'toolText.disablebitlockerSessionEnded')
     }
 }
