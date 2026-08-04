766b5f2d2410607e15fd1a89a6f08533ffaf7e71
Magnetarman <40738529+Magnetarman@users.noreply.github.com>
2026-08-04 20:47:12 +0200

    Update compiler.ps1

--- compiler.ps1
+++ compiler.ps1
@@ -56,7 +56,7 @@ function Initialize-SourceTextLocalization {
 function Get-SourceTextLoc {
     param(
         [Parameter(Mandatory = $true)][string]$Key,
-        [object[]]$Arguments = @()
+        [Alias('Args')][object[]]$Arguments = @()
     )
 
     $value = $null
